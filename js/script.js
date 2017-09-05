df0a4c0d489341b864c7859f23873da511e9bc6f
katsutaka <katsutaka.work@gmail.com>
2017-09-05 19:22:33 +0900

    practice002において、Javascriptの概要のページを作成

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 $(function(){
-
   $('.thisDeco').mouseover(function () {
     $(this).fadeOut("slow");
+  }),
+  $('.clickEventExe').click(function () {
+    $(this).text("クリックされました");
+  }),
+  $('.mouseoverEventExe').mouseover(function () {
+    $(this).text("マウスオーバーされました");
+  }),
+  $('.mouseoutEventExe').mouseout(function () {
+    $(this).text("マウスアウトされました");
   });
-
 });
