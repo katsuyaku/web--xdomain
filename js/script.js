6bdc0dcdd0c8a717f9240a79212869aa45e763a9
katsutaka <katsutaka.work@gmail.com>
2017-09-06 18:56:52 +0900

    jQueryにおける、mouseイベントのhtml作成

--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
 $(function(){
+
   $('.thisDeco').mouseover(function () {
     $(this).fadeOut("slow");
   }),
+
+
   $('.clickEventExe').click(function () {
     $(this).text("クリックされました");
+  }),
+  $('.dblclickEventExe').dblclick(function () {
+    $(this).text("ダブルクリックされました");
+  }),
+  $('.mousedownEventExe').mousedown(function () {
+    $(this).text("マウスダウンされました");
+  }),
+  $('.mouseupEventExe').mouseup(function () {
+    $(this).text("マウスアップされました");
   }),
   $('.mouseoverEventExe').mouseover(function () {
     $(this).text("マウスオーバーされました");
   }),
   $('.mouseoutEventExe').mouseout(function () {
     $(this).text("マウスアウトされました");
-  })
+  }),
+  $('.mouseenterEventExe').mouseenter(function () {
+    $(this).text("マウスエンターされました");
+  }),
+  $('.mouseleaveEventExe').mouseleave(function () {
+    $(this).text("マウスリーブされました");
+  }),
+  $('.mousemoveEventExe').mousemove(function () {
+    $(this).text("マウスムーブされました");
+  }),
+  $('.mousehoverEventExe').mouseout(function () {
+    $(this).text("マウスホバーされました");
+  });
 
 
 /*  $( ".ui-accordion" ).accordion();
 */
-
-
 });
